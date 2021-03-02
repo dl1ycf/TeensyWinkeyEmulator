1128a3a5b170f2d799c0032504cea3b25e79745c
c vw <dl1ycf@darc.de>
2021-03-02 16:24:22 +0100

    changed comments slightly

--- pins.h
+++ pins.h
@@ -1,18 +1,20 @@
 //
 // Hardware lines.
-// For simple Arduinos, you cannot use 0 and 1 (hardware serial line),
-// and you can only use 2 and 3 for Paddle (since they should trigger interrupts)
+// For simple Arduinos, you cannot use 0 and 1 (hardware serial line)
+//
+// Input pins that you do not connect should not be defined. This applies to
+// StraightKey, POTPIN, VOLPIN
 //
 
 #define PaddleRight         0      // input for right paddle
 #define PaddleLeft          1      // input for left paddle
-#define StraightKey         2      // input for straight key (only used with the STRAIGHT_KEY feature)
+#define StraightKey         2      // input for straight key
 
 #define CWOUT               9      // CW keying output (active high)
 #define PTTOUT              13     // PTT output (active high) (and built-in LED)
 
-#define POTPIN              A0     // analog input pin for the Speed pot, do not define if not connected
-#define VOLPIN              A1     // analog input pin for the volume pot, do not define if not connected
+#define POTPIN              A0     // analog input pin for the Speed pot
+#define VOLPIN              A1     // analog input pin for the volume pot
 
 #define RSPIN               3      // LCD display RS (only used with the LCDDISPLAY feature)
 #define ENPIN               4      // LCD display EN (only used with the LCDDISPLAY feature)
