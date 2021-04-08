a0d806745e6008584b428a2648786d2851e0975b
c vw <dl1ycf@darc.de>
2021-04-08 15:13:28 +0200

    Removed LCD feature define that was not active anyway

--- config.h
+++ config.h
@@ -10,11 +10,9 @@
 #define OPTION_SIDETONE__VOLUME    0.2  // initial side tone volume
 #define OPTION_SIDETONE_FREQ       800  // initial side tone frequency
 
-//#define FEATURE_LCDDISPLAY            // define to support a small 2*16 char LCD display
-
 //
-// Notes:
-// ======
+// Default values:
+// ===============
 //
 // If OPTION_MIDI_CW_CHANNEL      is not defined, channel 1 will be used
 // If OPTION_MIDI_CW_NOTE         is not defined, Note #1 is used for MIDI CW-key messages
@@ -23,6 +21,4 @@
 // If OPTION_AUDIO_MQS            is not defined the I2S audio device will be used
 // If OPTION_SIDETONE_VOLUME      is not defined 0.2 will be used
 // If OPTION_SIDETONE_FREQ        is not defined 600 Hz will be used
-//
-// Features not defined will not be compiled. The only selectable feature is LCD support.
 //
