5b85bf3d4d1db88a6794d149d6a277e869d39737
DL1YCF <dl1ycf@darc.de>
2022-02-06 23:02:47 +0100

    Use only with the CWKeyerShield since "usb_names.h" only exists for Teensy3/4

--- USBname.c
+++ USBname.c
@@ -2,30 +2,19 @@
 // placed into a .c file (its own tab).  It can not be in
 // a .cpp file or your main sketch (the .ino file).
 
+// Only works with Teensy3 and Teensy4.
+
 #include "config.h"
 
-#ifdef CWKEYERSHIELD
-#undef USBMIDI
-#endif
 
-#if defined(CWKEYERSHIELD) || defined(USBMIDI)
+#ifdef CWKEYERSHIELD
+
 #include "usb_names.h"
-#endif
 
-#ifdef CWKEYERSHIELD
 // Name = "KeyerShield"
 #define MIDI_NAME   {'K', 'e', 'y', 'e', 'r', 'S', 'h', 'i', 'e', 'l', 'd'}
 #define MIDI_NAME_LEN  11
-#endif
 
-#ifdef USBMIDI
-// Name = "Keyer"
-#define MIDI_NAME   {'K', 'e', 'y', 'e', 'r'}
-#define MIDI_NAME_LEN 5
-#endif
-
-
-#if defined(CWKEYERSHIELD) || defined(USBMIDI)
 // Do not change this part.  This exact format is required by USB.
 struct usb_string_descriptor_struct usb_string_product_name = {
         2 + MIDI_NAME_LEN * 2,
