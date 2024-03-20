64e7af9736c53b1e36036cfb040017859ac1f4d1
c vw <dl1ycf@darc.de>
2024-03-20 12:15:32 +0100

    Edited config.h such that it defaults to the CW Keyer Shield

--- config.h
+++ config.h
@@ -6,11 +6,12 @@
 // See the included file for a description which hardware and
 // which operation mode is implemented there.
 //
+//
 ////////////////////////////////////////////////////////////////////////////
 
-#include "config.32U4.h"
+//#include "config.32U4.h"
 //#include "config.arduino.h"
-//#include "config.keyershield.h"
+#include "config.keyershield.h"
 //#include "config.keyershield_sgtl5000.h"
 //#include "config.teensy.h"
 //#include "config.teensy4_bare.h"
